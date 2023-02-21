d57b8299fc5dad26fed8fcdb55b937e8fb8de2cf
francesco Aduni <francesco.aduni@avanade.com>
2023-02-21 16:39:50 +0100

    esercizio3

---
+++ esercizio3/src/app/dipendente/dipendente.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dipendente',
+  templateUrl: './dipendente.component.html',
+  styleUrls: ['./dipendente.component.css']
+})
+export class DipendenteComponent implements OnInit {
+
+  nascosto:boolean=true;
+  onClick():void{
+    this.nascosto= this.nascosto? false:true;
+  }
+  dipendente:any = {
+    "codice": 23,
+    "immagine": 'assets/img/23.jpg',
+    "cognome": "Persoldi",
+    "nome": "Davide",
+    "assunzione": "10-12-2018",
+    "indirizzo": "via Agra 23",
+    "cap": "80143",
+    "citta": "Napoli",
+    "telefono": "3473498004",
+    "email": "davide.persoldi@gmail.com"
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
